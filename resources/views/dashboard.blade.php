c4ce6928e62f5fb55ad6318735a835f52c4ce5e9
glen morse <morse911@outlook.com>
2023-03-05 20:19:17 +0000

    dashboard.blade.php edited online with Bitbucket test edit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Book It Dashboard</title>
-  <link rel="stylesheet" href="CSS/Dashboardcss.css">
+ <link rel="stylesheet" href="{{ asset('css/Dashboardcss.css') }}">
 </head>
 <body>
   <header>

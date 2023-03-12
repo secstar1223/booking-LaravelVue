46bc42665851c7e67ec1c556dddc7d2dba73e8e8
glen morse <morse911@outlook.com>
2023-03-12 18:27:24 +0000

    nav.blade.php edited with basic id's and classes

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,1 +1,37 @@
-<h1>navbar </h1>
+<h2>Navigation</h2>
+	<div class="nav-button-container">
+		<button id="nav-dashboard-btn">Dashboard</button>
+		<ul id="nav-dashboard-menu" class="hidden">
+			<li><a href="#" id="nav-dash-manifest-btn">Manifest</a></li>
+			<li><a href="#" id="nav-dash-capacity-btn">Capacity Report</a></li>
+		</ul>
+		<button id="nav-calendar-btn">Calendar</button>
+		<ul id="nav-calendar-menu" class="hidden">
+			<li><a href="#" id="nav-cal-rentals-btn">Rentals</a></li>
+			<li><a href="#" id="nav-cal-avilablity-btn">Availablity</a></li>
+		</ul>
+		<button id="nav-customers-btn">Customers</button>
+		<ul id="nav-customers-menu" class="hidden">
+			<li><a href="#" id="nav-customers-search-btn">Search</a></li>
+			<li><a href="#" id="nav-customers-members-btn">Members</a></li>
+		</ul>
+		<button id="nav-reports-btn">Reports</button>
+		<ul id="nav-reports-menu" class="hidden">
+			<li><a href="#" id="nav-report-sales-btn">sales</a></li>
+			<li><a href="#" id="nav-report-profit-btn">profit</a></li>
+		</ul>
+		<button id="nav-marketing-btn">Marketing</button>
+		<ul id="nav-marketing-menu" class="hidden">
+			<li><a href="#"id="nav-market-promocode-btn">PromoCode</a></li>
+			<li><a href="abandon.html" id="nav-market-abandoned-btn">Abandoned</a></li>
+		</ul>
+		<button id="nav-products-btn">Products</button>
+		<ul id="nav-products-menu" class="hidden">
+			<li><a href="#"id="nav-products-rentals-btn">Rentals</a></li>
+			<li><a href="addon.html"id="nav-products-addon-btn">Add-ons</a></li>
+			<li><a href="#"id="nav-products-membership-btn">Memberships</a></li>
+		</ul>
+		<button id="nav-configuration-btn">Configuration</button>
+		<ul id="nav-configuration-menu" class="hidden">
+		</ul>
+	</div>

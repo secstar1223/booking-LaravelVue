e66282303ee5462b02ecdf04d2536848957a0c5b
glen morse <morse911@outlook.com>
2023-03-16 23:49:36 +0000

    Welcome.vue edited online with Bitbucket dam vue.. i had template 2 times.

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -40,7 +40,6 @@ export default {
                 <Link v-if="canRegister" :href="route('register')" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</Link>
             </template>
         </div> 
-        <template>
           <div class="landing-page">
 		    <header>
 		      <h1>Booking 24/7</h1>
@@ -67,7 +66,6 @@ export default {
 				  <p>&copy; 2023 Booking 24/7. All rights reserved.</p>
 		    </footer>
           </div>
-		</template>
 </template>
 
 

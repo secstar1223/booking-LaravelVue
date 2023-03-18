07a4d94382e73932b55372a0e66d24f164698151
glen morse <morse911@outlook.com>
2023-03-18 02:25:07 +0000

    Dashboard.vue edited online with Bitbucket

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -555,14 +555,6 @@ height: 32px;
 font-size: 18px;
 text-shadow: 0 1px 0 rgb(0 0 0 / 40%);
 transition: box-shadow 0.15s ease,transform 0.15s ease;
-:hover {
-	box-shadow: 0px 4px 8px rgb(45 35 66 / 40%), 0px 7px 13px -3px rgb(45 35 66 / 30%), inset 0px -3px 0px #3c4fe0;
-	transform: translateY(-2px);
-}
-:active{
-	box-shadow: inset 0px 3px 7px #3c4fe0;
-	transform: translateY(2px);
-}
 }
 
 .leftnav button:hover {

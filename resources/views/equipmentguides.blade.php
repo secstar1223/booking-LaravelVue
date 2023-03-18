1b583df9f1a6e6baaae3ac82e8b104220f869d95
glen morse <morse911@outlook.com>
2023-03-18 18:26:02 +0000

    equipmentguides.blade.php edited online with Bitbucket

--- resources/views/equipmentguides.blade.php
+++ resources/views/equipmentguides.blade.php
@@ -327,7 +327,7 @@ small {
 <div id="myModal" class="modal"> 
   <div class="modal-content">
     <h2>Edit Item</h2>
-    <form method="POST" action="{{ route('equipment.store') }}">
+    <form method="POST" action="{{ route('equipmentguides.store') }}">
       @csrf
       <fieldset>
         <legend>Item Name</legend>

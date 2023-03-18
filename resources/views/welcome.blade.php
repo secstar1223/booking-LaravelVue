cf61d6668c952e744b1efaf1e4319ece54b334af
glen morse <morse911@outlook.com>
2023-03-18 14:11:56 +0000

    welcome.blade.php edited online with Bitbucket

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,12 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Book It Dashboard</title>
+  <link rel="stylesheet" href="css/dashboardcss.css">
+</head>
+<body>
+
 @if($canLogin)
   <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
     @if(isset($auth['user']))
@@ -10,6 +19,123 @@
     @endif
   </div>
 @endif
+
+<style>
+/* Set background color and font */
+body {
+  background-color: #87CEEB; /* light blue */
+  font-family: 'Roboto', sans-serif;
+}
+
+/* Style the header section */
+header {
+  text-align: center;
+  margin-top: 100px;
+}
+
+h1 {
+  font-size: 64px;
+  color: #FFFFFF; /* white */
+  text-shadow: 2px 2px 5px #000000; /* add shadow */
+}
+
+p {
+  font-size: 24px;
+  color: #FFFFFF; /* white */
+  margin-bottom: 50px;
+}
+
+.cta-btn {
+  display: inline-block;
+  background-color: #F08080; /* coral */
+  color: #FFFFFF; /* white */
+  font-size: 20px;
+  padding: 20px 40px;
+  border-radius: 50px;
+  text-decoration: none;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.cta-btn:hover {
+  background-color: #FFA07A; /* light salmon */
+}
+
+/* Style the benefits section */
+.benefits {
+  text-align: center;
+  margin-top: 100px;
+}
+
+h2 {
+  font-size: 48px;
+  color: #FFFFFF; /* white */
+  text-shadow: 2px 2px 5px #000000; /* add shadow */
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  font-size: 24px;
+  color: #FFFFFF; /* white */
+  margin-bottom: 20px;
+}
+
+/* Style the testimonials section */
+.container {
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+.testimonial-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.testimonial {
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 5px 20px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  margin: 20px;
+  max-width: 400px;
+}
+
+.testimonial p {
+  font-size: 1.2rem;
+  line-height: 1.5;
+  color: #000;
+}
+
+.author {
+  text-align: right;
+  font-style: italic;
+}
+
+/* Style the footer */
+footer {
+  text-align: center;
+  margin-top: 100px;
+  color: #FFFFFF; /* white */
+}
+
+/* Set link styles */
+a {
+  color: #FFFFFF; /* white */
+  text-decoration: none;
+  transition: color 0.3s ease-in-out;
+}
+
+a:hover {
+  color: #FFA07A; /* light salmon */
+}
+
+</style>
+
 
 
 <div class="landing-page">
@@ -43,3 +169,5 @@
   const laravelVersion = '{{ App::version() }}';
   const phpVersion = '{{ PHP_VERSION }}';
 </script>
+</body>
+</html>

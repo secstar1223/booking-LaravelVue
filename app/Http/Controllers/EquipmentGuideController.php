ad5c7056bfe0136332acf85dcb288555b148653f
glen morse <morse911@outlook.com>
2023-03-18 18:42:15 +0000

    EquipmentGuideController.php edited online with Bitbucket

--- app/Http/Controllers/EquipmentGuideController.php
+++ app/Http/Controllers/EquipmentGuideController.php
@@ -1,24 +1,37 @@
-namespace App\Http\Controllers;
-
+use App\Models\Equipment;
 use Illuminate\Http\Request;
-use App\Models\EquipmentGuide;
 
 class EquipmentGuideController extends Controller
 {
     public function store(Request $request)
     {
-        $equipmentGuide = new EquipmentGuide();
-        $equipmentGuide->name = $request->input('name');
-        $equipmentGuide->short_name = $request->input('short_name');
-        $equipmentGuide->color = $request->input('color');
-        $equipmentGuide->quantity = $request->input('quantity');
-        $equipmentGuide->capacity = $request->input('capacity');
-        $equipmentGuide->resource_tracking = $request->input('resource_tracking', false);
-        $equipmentGuide->description = $request->input('description');
+        // Validate form data
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'short_name' => 'required|max:10',
+            'color' => 'required',
+            'quantity' => 'required|integer|min:0',
+            'capacity' => 'required|integer|min:0',
+            'description' => 'required',
+        ]);
 
-        $equipmentGuide->save();
+        // Create a new Equipment model with the validated data
+        $equipment = new Equipment([
+            'name' => $validatedData['name'],
+            'short_name' => $validatedData['short_name'],
+            'color' => $validatedData['color'],
+            'quantity' => $validatedData['quantity'],
+            'capacity' => $validatedData['capacity'],
+            'description' => $validatedData['description'],
+            'resource_tracking' => $request->input('resource_tracking', false),
+        ]);
 
-        return redirect('/equipmentguides')->with('success', 'Equipment guide created successfully.');
+        // Save the Equipment model to the database
+        $equipment->save();
+
+        // Redirect the user back to the form
+        return back();
     }
 }
+
 

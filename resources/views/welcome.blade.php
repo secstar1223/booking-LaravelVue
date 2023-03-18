3c1297e04c3d82cbf20f2889920becb0c36f38d7
glen morse <morse911@outlook.com>
2023-03-18 13:53:32 +0000

    welcome.blade.php edited online with Bitbucket

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,5 @@
 
-<x-head title="Welcome"/>
 
-<div v-if="{{ $canLogin }}" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
-    @if (Auth::check())
-        <a href="{{ route('dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-    @else
-        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-
-        @if ($canRegister)
-            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-        @endif
-    @endif
-</div>
 
 <div class="landing-page">
     <div class="container">

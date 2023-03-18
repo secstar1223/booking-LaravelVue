c9480d49b5a07a3d71ef02b5a38dcbf27ad46847
glen morse <morse911@outlook.com>
2023-03-18 22:02:55 +0000

    2023_03_18_163922_create_equipment_table.php edited online with Bitbucket

--- database/migrations/2023_03_18_163922_create_equipment_table.php
+++ database/migrations/2023_03_18_163922_create_equipment_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
 public function up()
 {
-    Schema::create('equpment', function (Blueprint $table) {
+    Schema::create('equipment', function (Blueprint $table) {
         $table->id();
         $table->string('name');
         $table->string('short_name', 10);

ffe7c5b66d216befba60c6df2c306366ac19fa4e
glen morse <morse911@outlook.com>
2023-03-18 17:03:55 +0000

    EquipmentController.php created online with Bitbucket

---
+++ app/Http/Controllers/EquipmentController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Equipment;
+
+class EquipmentController extends Controller
+{
+    public function create()
+    {
+        return view('equipment.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Validate the form data
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'short_name' => 'required|max:10',
+            'color' => 'required',
+            'quantity' => 'required|numeric|min:0',
+            'capacity' => 'required|numeric|min:0',
+            'description' => 'required',
+        ]);
+
+        // Save the equipment to the database
+        $equipment = new Equipment;
+        $equipment->name = $validatedData['name'];
+        $equipment->short_name = $validatedData['short_name'];
+        $equipment->color = $validatedData['color'];
+        $equipment->quantity = $validatedData['quantity'];
+        $equipment->capacity = $validatedData['capacity'];
+        $equipment->description = $validatedData['description'];
+        $equipment->resource_tracking = $request->input('resource_tracking', false);
+        $equipment->save();
+
+        // Redirect to the equipment listing page
+        return redirect()->route('equipment.index')
+                         ->with('success', 'Equipment created successfully!');
+    }
+
+    public function index()
+    {
+        $equipment = Equipment::all();
+        return view('equipment.index', compact('equipment'));
+    }
+}

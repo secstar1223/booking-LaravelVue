4d32358702126ad7b0780e593fdd28c5f2a38b1a
glen morse <morse911@outlook.com>
2023-03-19 14:37:05 +0000

    web.php edited online with Bitbucket

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::middleware([
 
 Route::get('/equipmentguides', [EquipmentGuidesController::class, 'index']);
 Route::post('/equipmentguides', [EquipmentGuidesController::class, 'store'])->name('equipmentguides.store');
-Route::get('/equipmentguides/{id}', [EquipmentGuidesController::class, 'show']);
+
 
 
 

48b6136366ded48dfec674e8c334530b020cfc05
glen morse <morse911@outlook.com>
2023-03-20 00:34:51 +0000

    NewRentalController.php created online with Bitbucket

---
+++ app/Http/Controllers/NewRentalController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Equipment;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class NewRentalController extends Controller
+{
+        public function store(Request $request)
+    {
+        $product = new Product;
+        $product->name = $request->input('product-name');
+        $product->description = $request->input('description');
+        // Upload the image file and save the path to the database
+        $product->image = 'path/to/image'; 
+        $product->save();
+
+        return redirect('/')->with('success', 'Product created successfully.');
+    }
+
+}
+
+

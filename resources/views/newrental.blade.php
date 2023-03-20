731bd370cefc16293990c39787fc28ae15b0ac33
glen morse <morse911@outlook.com>
2023-03-20 00:53:09 +0000

    newrental.blade.php edited online with Bitbucket

--- resources/views/newrental.blade.php
+++ resources/views/newrental.blade.php
@@ -526,24 +526,24 @@ input[type="color"] {
 		<div class="right-content">
           <h2 class="edit-product-heading">Edit Product</h2>
           <form class="product-form" action="/newrental" method="POST">
-           @csrf
-            <div class="form-group">
-        <label for="product-name" class="form-label">Product Name:</label>
-        <input type="text" id="product-name" name="product-name" class="form-input">
-      </div>
-      <div class="form-group">
-        <label for="description" class="form-label">Description:</label>
-        <input type="text" id="description" name="description" class="form-input">
-      </div>
-      <div class="form-group">
-        <label for="widget-image" class="form-label">Widget Image:</label>
-        <button class="change-image-button">Change Image</button>
-      </div>
-      <button class="delete-product-button">Delete this product</button>
-      <button type="submit" class="submit-product-button">Submit</button>
-    </form>
-  </div>
-</div>
+             @csrf
+              <div class="form-group">
+                <label for="product-name" class="form-label">Product Name:</label>
+                <input type="text" id="product-name" name="product-name" class="form-input">
+              </div>
+              <div class="form-group">
+                <label for="description" class="form-label">Description:</label>
+                <input type="text" id="description" name="description" class="form-input">
+              </div>
+              <div class="form-group">
+                <label for="widget-image" class="form-label">Widget Image:</label>
+                <button class="change-image-button">Change Image</button>
+              </div>
+              <button class="delete-product-button">Delete this product</button>
+              <button type="submit" class="submit-product-button">Submit</button>
+            </form>
+        </div>
+    </div>
 
 <!--Equipment button on menu -->
       <div id="equipment" class="hidden">

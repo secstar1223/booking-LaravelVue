050cd1f1899bd39519ec0286100b2224a54caf54
glen morse <morse911@outlook.com>
2023-03-21 22:32:06 +0000

    NewRentalController.php edited online with Bitbucket

--- app/Http/Controllers/NewRentalController.php
+++ app/Http/Controllers/NewRentalController.php
@@ -1,7 +1,8 @@
 <?php
 namespace App\Http\Controllers;
 
-use App\Models\Product;
+use App\Models\Rental_Product;
+use App\Models\Rental_Equipment_Type;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -24,6 +25,39 @@ class NewRentalController extends Controller
 
         return redirect('/newrental');
     }
+    
+    
+	public function rentalequipmenttype(Request $request)
+    {
+        $display_name = $request->input('display_name');
+        $equipment_pool = $request->input('equipment_pool');
+        $description = $request->input('description');
+        $widget_image = $request->input('widget_image');
+        $widget_display = $request->input('widget_display');
+        $min_value = $request->input('min_value');
+        $max_value = $request->input('max_value');
+        $require_min = $request->input('require_min');
+        $category = $request->input('category');
+        
+        $Rental_Equipment_Type = new Rental_Equipment_type;
+        $Rental_Equipment_Typet->display_name = $display_name;
+        $Rental_Equipment_Type->equipment_pool = $equipment_pool;
+        $Rental_Equipment_Type->description = $description;
+        $Rental_Equipment_Type->widget_image = $widget_image;
+        $Rental_Equipment_Type->widget_display = $widget_display;
+        $Rental_Equipment_Type->min_value = $min_value;
+        $Rental_Equipment_Type->max_value = $max_value;
+        $Rental_Equipment_Type->require_min = $require_min;
+        $Rental_Equipment_Type->category = $category;
+		
+        $Rental_Equipment_Type->save();
+
+        return redirect('/newrental');
+    }
+	    
+    
+    
+    
 }
 
 

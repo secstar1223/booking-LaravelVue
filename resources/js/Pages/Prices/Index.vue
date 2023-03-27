5c5239f8053bf45cc08df55b46ee5ddf4b80d910
glen morse <morse911@outlook.com>
2023-03-27 20:56:39 +0000

    Index.vue edited online with Bitbucket

--- resources/js/Pages/Prices/Index.vue
+++ resources/js/Pages/Prices/Index.vue
@@ -1,10 +1,9 @@
-
 <script setup>
 import { reactive } from 'vue'
 import AppLayout from '@/Layouts/AppLayout.vue';
 
 const props = defineProps({
-    rentalProductId: {
+    detailsId: {
         type: Number,
         required: true
     },
@@ -12,35 +11,27 @@ const props = defineProps({
         type: Array,
         required: true
     }
+	equipmenttype: {
+        type: Array,
+        required: true
+    }
 })
 
-function addDuration() {
-    state.durations.push({
-        id: Date.now(),
-        name: '',
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        has_changes: false,
-        is_new: true,
-        errors: [],
-    })
-}
 
-function saveDuration(durationId) {
-    let duration = state.durations.filter((duration) => (duration.id == durationId))
-    if (duration.length != 1) {
+function savePrice(priceId) {
+    let price = state.price.filter((price) => (price.id == priceId))
+    if (price.length != 1) {
         return
     }
 
-    duration = duration[0]
+    price = price[0]
 
     let method = 'PUT'
-    let url = '/rentals/' + props.rentalProductId + '/durations/' + duration.id
+    let url = '/rentals/' + props.detailsId + '/price/' + price.id
 
-    if (duration.is_new) {
+    if (price.is_new) {
         method = 'POST'
-        url = '/rentals/' + props.rentalProductId + '/durations'
+        url = '/rentals/' + props.detailsId + '/price'
     }
 
     var status = 0
@@ -51,7 +42,7 @@ function saveDuration(durationId) {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         },
-        body: JSON.stringify(duration),
+        body: JSON.stringify(price),
     })
     .then((response) => {
         if (response.status != 200) {
@@ -61,107 +52,67 @@ function saveDuration(durationId) {
         return response.json()
     })
     .then((data) => {
-        if (data.durations) {
-            state.durations = data.durations
+        if (data.price) {
+            state.price = data.price
         }
     })
     .catch((response) => {
         return response.json().then((data) => {
-            duration.errors = data.errors
-        });
-    })
-}
-
-function deleteDuration(durationId) {
-    let duration = state.durations.filter((duration) => (duration.id == durationId))
-    if (duration.length != 1) {
-        return
-    }
-
-    duration = duration[0]
-
-    let method = 'DELETE'
-    let url = '/rentals/' + props.rentalProductId + '/durations/' + duration.id
-
-    fetch(url, {
-        method,
-        headers: {
-            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf"]').content,
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-    })
-    .then((response) => {
-        if (response.status != 200) {
-            throw response
-        }
-
-        return response.json()
-    })
-    .then((data) => {
-        if (data.durations) {
-            state.durations = data.durations
-        }
-    })
-    .catch((response) => {
-        return response.json().then((data) => {
-            duration.errors = data.errors
+            price.errors = data.errors
         });
     })
 }
 
 const state = reactive({
-    durations: props.durations,
+    price: props.price,
 })
 </script>
 
 <template>
-<AppLayout title="Durations">
+<AppLayout title="Prices">
     <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Durations
+                Prices
             </h2>
     </template>
-    <div>
-        <table style="width:100%;">
-            <tbody>
-                <tr v-for="duration in state.durations" :key="duration.id">
-                    <td>
-                        <label>Name:</label>
-                        <input type="text" v-model="duration.name">
-                        <div v-if="duration.errors.name">
-                            <div v-for="(item, key) in duration.errors.name" :key="key">{{ item }}</div>
-                        </div>
-                    </td>
-                    <td>
-                        <label>Days:</label>
-                        <input type="text" v-model="duration.days">
-                        <div v-if="duration.errors.days">
-                            <div v-for="(item, key) in duration.errors.days" :key="key">{{ item }}</div>
-                        </div>
-                    </td>
-                    <td>
-                        <label>Hours:</label>
-                        <input type="text" v-model="duration.hours">
-                        <div v-if="duration.errors.hours">
-                            <div v-for="(item, key) in duration.errors.hours" :key="key">{{ item }}</div>
-                        </div>
-                    </td>
-                    <td>
-                        <label>Minutes:</label>
-                        <input type="text" v-model="duration.minutes">
-                        <div v-if="duration.errors.minutes">
-                            <div v-for="(item, key) in duration.errors.minutes" :key="key">{{ item }}</div>
-                        </div>
-                    </td>
-                    <td>
-                        <button @click="saveDuration(duration.id)">Save</button>
-                        <button @click="deleteDuration(duration.id)">Delete</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        <button @click="addDuration">Add a duration</button>
+ <div class="overflow-x-auto">
+      <table class="table-auto">
+        <thead>
+          <tr>
+            <th></th> <!-- Empty header for spacing -->
+            <template v-for="equipmenttype in props.equipmenttype">
+              <th>{{ equipmenttype.name }}</th>
+            </template>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="duration in props.durations" :key="duration.id">
+            <td>{{ duration.name }}</td>
+            <template v-for="equipmenttype in props.equipmenttype">
+              <td>
+                <div>
+                  <label>Price:</label>
+                  <input type="text" v-model="duration.prices[equipmenttype.id].price">
+                  <div v-if="duration.prices[equipmenttype.id].errors.price">
+                    <div v-for="(item, key) in duration.prices[equipmenttype.id].errors.price" :key="key">{{ item }}</div>
+                  </div>
+                </div>
+
+                <div>
+                  <label>Deposit:</label>
+                  <input type="text" v-model="duration.prices[equipmenttype.id].deposit">
+                  <div v-if="duration.prices[equipmenttype.id].errors.deposit">
+                    <div v-for="(item, key) in duration.prices[equipmenttype.id].errors.deposit" :key="key">{{ item }}</div>
+                  </div>
+                </div>
+
+                <button @click="savePrice(duration.prices[equipmenttype.id].id)">Save</button>
+              </td>
+            </template>
+          </tr>
+        </tbody>
+      </table>
     </div>
 </AppLayout>
 </template>

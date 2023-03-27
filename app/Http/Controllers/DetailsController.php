dc1e7f035e425675520a15df38579a9cb19e2a12
glen morse <morse911@outlook.com>
2023-03-27 20:44:56 +0000

    DetailsController.php created online with Bitbucket

---
+++ app/Http/Controllers/DetailsController.php
@@ -0,0 +1,94 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Details;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+
+class DetailsController extends Controller
+{
+    public function index()
+    {
+        $user = auth()->user();
+        $team = $user->currentTeam;
+        $details = $team->details;
+
+        return Inertia::render('Details/Index', [
+            'details' => $details,
+        ]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('Details/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'image' => 'required|image|mimes:jpeg,png,gif|max:2048',
+        ]);
+
+        $teamId = auth()->user()->currentTeam->id;
+    
+        $detail = new Detail();
+        $detail->name = $validatedData['name'];
+        $detail->description = $validatedData['description'];
+        $detail->image = $validatedData['image'];
+        $detail->team_id = $teamId;
+        $detail->save();
+
+        return redirect()->route('details.index');
+    }
+
+    public function edit(Detail $detail)
+    {
+        return Inertia::render('Details/Edit', [
+            'detail' => $detail,
+        ]);
+    }
+
+    public function update(Request $request, Detail $detail)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'image' => 'required|image|mimes:jpeg,png,gif|max:2048',
+        ]);
+
+        $user = auth()->user();
+        $team = $user->currentTeam;
+
+        if (!$team->details()->where('id', $detail->id)->exists()) {
+            return redirect()->back()->withErrors(['error' => 'The specified details does not exist or is not associated with the current team.']);
+        }
+
+        $detail->name = $validatedData['name'];
+        $detail->description = $validatedData['description'];
+        $detail->image = $validatedData['image'];
+        $detail->save();
+
+        return redirect()->route('details.index');
+    }
+
+    public function destroy(Detail $detail)
+    {
+        $user = auth()->user();
+        $team = $user->currentTeam;
+
+        if (!$team->details()->where('id', $detail->id)->exists()) {
+            return redirect()->back()->withErrors(['error' => 'The specified detail does not exist or is not associated with the current team.']);
+        }
+
+        try {
+            $detail->delete();
+        } catch (\Exception $e) {
+            return redirect()->back()->withErrors(['error' => 'Unable to delete detail.']);
+        }
+
+        return redirect()->route('details.index');
+    }
+}

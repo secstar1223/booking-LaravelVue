a3aee9b17389d45e0dd15202fba4485238f73842
glen morse <morse911@outlook.com>
2023-03-27 21:03:04 +0000

    2023_03_44_100000_create_prices_table.php edited online with Bitbucket

--- database/migrations/2023_03_44_100000_create_prices_table.php
+++ database/migrations/2023_03_44_100000_create_prices_table.php
@@ -13,18 +13,15 @@ return new class extends Migration
             $table->unsignedBigInteger('duration_id');
             $table->foreign('duration_id')->references('id')->on('durations')->onDelete('cascade');
             $table->index('duration_id');
-            $table->unsignedBigInteger('equipment_id');
-            $table->foreign('equipment_id')->references('id')->on('equipment')->onDelete('cascade');
-            $table->index('equipment_id');
-            $table->unsignedBigInteger('tax_group_id');
-            $table->foreign('tax_group_id')->references('id')->on('tax_groups')->onDelete('cascade');
-            $table->index('tax_group_id');
+            $table->unsignedBigInteger('equipmenttype_id');
+            $table->foreign('equipmenttype_id')->references('id')->on('equipmenttype')->onDelete('cascade');
+            $table->index('equipmenttype_id');
+            $table->unsignedBigInteger('details_id');
+            $table->foreign('details_id')->references('id')->on('details')->onDelete('cascade');
+            $table->index('details_id');
 
             $table->integer('total');
             $table->integer('deposit');
-            $table->integer('tax');
-            $table->enum('tax_type', ['fixed', 'percent']);
-            $table->string('currency');
 
             $table->timestamps();
         });

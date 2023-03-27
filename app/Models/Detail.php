beba92598931fc304f40de17913e9c566bcb178e
glen morse <morse911@outlook.com>
2023-03-27 23:07:31 +0000

    Detail.php edited online with Bitbucket

--- app/Models/Detail.php
+++ app/Models/Detail.php
@@ -4,7 +4,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Detail extends Model {
-	 protected $table = 'rental_products';
+	 protected $table = 'detail';
 	 
     protected $fillable = [
         'name',

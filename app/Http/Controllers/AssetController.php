c11624c413fbd9fa10870e2d87d8e2a9438304a6
glen morse <morse911@outlook.com>
2023-03-27 01:31:47 +0000

    AssetController.php edited online with Bitbucket

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -12,10 +12,10 @@ class AssetController extends Controller
     {
         $user = auth()->user();
         $team = $user->currentTeam;
-        $asset = $team->assets;
+        $assets = $team->assets;
 
         return Inertia::render('Asset/Index', [
-            'assets' => $asset,
+            'assets' => $assets,
         ]);
     }
 

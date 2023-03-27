b73b5f7293b810249de7e82530e41dac41b8e1bd
glen morse <morse911@outlook.com>
2023-03-27 21:01:23 +0000

    2023_03_21_223929_create_rental_equipment_types_table.php edited online with Bitbucket

--- database/migrations/2023_03_21_223929_create_rental_equipment_types_table.php
+++ database/migrations/2023_03_21_223929_create_rental_equipment_types_table.php
@@ -15,15 +15,15 @@ return new class extends Migration
     {
         Schema::create('rental_equipment_types', function (Blueprint $table) {
             $table->id();
-            $table->string('display_name');
-            $table->string('equipment_pool');
+            $table->string('name');
+            $table->string('assetID');
+			$table->string('detailID');
             $table->text('description')->nullable();
             $table->string('widget_image')->nullable();
             $table->boolean('widget_display')->default(false);
-            $table->integer('min_value')->nullable();
-            $table->integer('max_value')->nullable();
+            $table->integer('min_amount')->nullable();
+            $table->integer('max_amount')->nullable();
             $table->boolean('require_min')->default(false);
-            $table->string('category')->nullable();
             $table->timestamps();
         });
     }
